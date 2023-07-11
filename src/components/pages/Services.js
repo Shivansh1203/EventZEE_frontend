7711c2fefeff22536c5b3ae84c2ed65241239daa
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-07-11 21:18:36 +0530

    push

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import '../../App.css';
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  const history = useHistory();
+
+  const handleButtonClick = () => {
+    // Redirect to another website
+    window.location.href = 'https://shivansh1203.github.io/Shivansh1203-Calibriya-E-Library-Project/';
+  };
+
+  return (
+    <div className='services'>
+      <button className='big-button' onClick={handleButtonClick}>
+        Go to Tech Event Website
+      </button>
+    </div>
+  );
 }
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+    console.log('Form submitted:', { name, email, password });
+  };
+
+  return (
+    <form className='sign-up' onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div>
+        <label htmlFor='name'>Name:</label>
+        <input
+          type='text'
+          id='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor='email'>Email:</label>
+        <input
+          type='email'
+          id='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor='password'>Password:</label>
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type='submit'>Submit</button>
+    </form>
+  );
 }
